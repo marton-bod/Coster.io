cbbab743a89383030f84f18edb1bf5b203ef3679
Marton Bod <marton.bod@gmail.com>
2019-04-13 22:43:11 +0200

    remove unnecessary scripts

--- scripts/runServices.sh
+++
@@ -1,7 +0,0 @@
-cd Coster.io/scripts
-docker-compose up -d
-
-cd ../../coster-frontend
-echo "Now launching frontend locally. You can shut it down by pressing CTRL + C."
-echo "To shut down the backend services, run the stopServices script."
-npm run dev
--- scripts/stopServices.sh
+++
@@ -1,4 +0,0 @@
-echo "Shutting down Spring boot apps."
-cd Coster.io/scripts
-docker-compose down
-echo "Shut down complete."
